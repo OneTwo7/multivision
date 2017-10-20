fc166959fd050df3300a32baa5995b5053e05945
onetwo7 <lahoohal@gmail.com>
2017-10-20 12:14:01 +0300

    Tiny fix

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -32,7 +32,7 @@ exports.createUser = function (req, res, next) {
 exports.updateUser = function (req, res) {
   var userUpdates = req.body;
 
-  if (parseInt(req.user._id) !== parseInt(userUpdates._id) && !req.user.hasRole('admin')) {
+  if (req.user._id.toString() !== userUpdates._id && !req.user.hasRole('admin')) {
     res.status(403);
     return res.end();
   }
